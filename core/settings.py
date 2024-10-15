912b4627dc6e1c63c7a831a598ceb87889c54cfc
efortish <91025555+efortish@users.noreply.github.com>
2024-10-15 01:18:51 -0500

    55

--- core/settings.py
+++ core/settings.py
@@ -223,7 +223,7 @@ if not DEBUG:
     AWS_SECRET_ACCESS_KEY =env('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME=env('AWS_STORAGE_BUCKET_NAME')
 
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.us-east-2.amazonaws.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.us-east-2.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl':'max-age-86400'}
     AWS_DEFAULT_ACL='public-read'
 
