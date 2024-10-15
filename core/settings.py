d88f715a6a84d26f3bcefa70854e4aa7c13ebba2
efortish <91025555+efortish@users.noreply.github.com>
2024-10-15 01:01:06 -0500

    otrocommit

--- core/settings.py
+++ core/settings.py
@@ -231,7 +231,7 @@ if not DEBUG:
 
     STATIC_LOCATION = 'static'
     STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATIC_LOCATION}/'
-    STATICFILES_STORAGE = 'storages.backends.s3boto.S3Boto3Storage'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
     #s3 public media settings
 
